9733bac8872165c33bfb0de4089e1871244ab7c6
teruzuki <yuj19@pitt.edu>
2016-11-08 23:54:37 -0500

    Yuefu Jiang Quiz 2

--- js/quiz2.js
+++ js/quiz2.js
@@ -4,12 +4,40 @@
 //  - create a button in the html that, once clicked, will fire off the function described in the next buller point
 //  - write a function that will fade out all of the html inside the body tag and replace it with a custom goodbye message when you click the button
 //  - finally, write a function that makes you happy :D (as in, have some fun a write something cool, it's up to you!)
+(function($){
+})(jQuery);
 
 (function($){
 	$('.links li a').on('click', function(e) {
 		e.preventDefault()
-		alrt('you clicked a link, good for you');
+		alert('you clicked a link, good for you');
 	});
 
 	console.log("this is a message for you!!!")
 })(jQery);
+
+
+function changeColor() {
+	var i;
+	var item = document.getElementById("orderedList");
+	for(i=0; i < orderedList.length; i++)
+	{
+		item[i].style.color = "blue";
+	}
+
+}
+
+function Goodbye() {
+	document.body.innerHTML = "Bye! It's a good time for some mayham";
+}
+
+function accordion()
+{
+		var accordion = document.getElementsByClassName("accordion");
+		var i;
+		for (i=0; i<accordion.length; i++) {
+    		accordion[i].onclick = function(){
+
+  			}
+  		}
+}
